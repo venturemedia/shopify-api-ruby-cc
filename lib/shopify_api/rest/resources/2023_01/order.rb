ece030585f2b06c3a38b61aec4cb191ef7a12402
Gerardo Ortega <g3ortega@gmail.com>
2023-01-20 15:08:22 -0600

    Fix order type error for subtotal_price

--- lib/shopify_api/rest/resources/2023_01/order.rb
+++ lib/shopify_api/rest/resources/2023_01/order.rb
@@ -74,7 +74,7 @@ module ShopifyAPI
       @source_identifier = T.let(nil, T.nilable(String))
       @source_name = T.let(nil, T.nilable(String))
       @source_url = T.let(nil, T.nilable(String))
-      @subtotal_price = T.let(nil, T.nilable(Float))
+      @subtotal_price = T.let(nil, T.nilable(String))
       @subtotal_price_set = T.let(nil, T.nilable(T::Hash[T.untyped, T.untyped]))
       @tags = T.let(nil, T.nilable(String))
       @tax_lines = T.let(nil, T.nilable(T::Array[T.untyped]))
@@ -98,181 +98,259 @@ module ShopifyAPI
     end
 
     @has_one = T.let({
-      customer: Customer
+      customer: Customer,
     }, T::Hash[Symbol, Class])
     @has_many = T.let({
       discount_codes: DiscountCode,
       fulfillments: Fulfillment,
-      refunds: Refund
+      refunds: Refund,
     }, T::Hash[Symbol, Class])
     @paths = T.let([
-      {http_method: :delete, operation: :delete, ids: [:id], path: "orders/<id>.json"},
-      {http_method: :get, operation: :count, ids: [], path: "orders/count.json"},
-      {http_method: :get, operation: :get, ids: [], path: "orders.json"},
-      {http_method: :get, operation: :get, ids: [:id], path: "orders/<id>.json"},
-      {http_method: :post, operation: :cancel, ids: [:id], path: "orders/<id>/cancel.json"},
-      {http_method: :post, operation: :close, ids: [:id], path: "orders/<id>/close.json"},
-      {http_method: :post, operation: :open, ids: [:id], path: "orders/<id>/open.json"},
-      {http_method: :post, operation: :post, ids: [], path: "orders.json"},
-      {http_method: :put, operation: :put, ids: [:id], path: "orders/<id>.json"}
+      { http_method: :delete, operation: :delete, ids: [:id], path: "orders/<id>.json" },
+      { http_method: :get, operation: :count, ids: [], path: "orders/count.json" },
+      { http_method: :get, operation: :get, ids: [], path: "orders.json" },
+      { http_method: :get, operation: :get, ids: [:id], path: "orders/<id>.json" },
+      { http_method: :post, operation: :cancel, ids: [:id], path: "orders/<id>/cancel.json" },
+      { http_method: :post, operation: :close, ids: [:id], path: "orders/<id>/close.json" },
+      { http_method: :post, operation: :open, ids: [:id], path: "orders/<id>/open.json" },
+      { http_method: :post, operation: :post, ids: [], path: "orders.json" },
+      { http_method: :put, operation: :put, ids: [:id], path: "orders/<id>.json" },
     ], T::Array[T::Hash[String, T.any(T::Array[Symbol], String, Symbol)]])
 
     sig { returns(T.nilable(T::Array[T::Hash[T.untyped, T.untyped]])) }
     attr_reader :line_items
+
     sig { returns(T.nilable(Integer)) }
     attr_reader :app_id
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :billing_address
+
     sig { returns(T.nilable(String)) }
     attr_reader :browser_ip
+
     sig { returns(T.nilable(T::Boolean)) }
     attr_reader :buyer_accepts_marketing
+
     sig { returns(T.nilable(String)) }
     attr_reader :cancel_reason
+
     sig { returns(T.nilable(String)) }
     attr_reader :cancelled_at
+
     sig { returns(T.nilable(String)) }
     attr_reader :cart_token
+
     sig { returns(T.nilable(String)) }
     attr_reader :checkout_token
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :client_details
+
     sig { returns(T.nilable(String)) }
     attr_reader :closed_at
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :company
+
     sig { returns(T.nilable(String)) }
     attr_reader :created_at
+
     sig { returns(T.nilable(String)) }
     attr_reader :currency
+
     sig { returns(T.nilable(String)) }
     attr_reader :current_subtotal_price
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :current_subtotal_price_set
+
     sig { returns(T.nilable(String)) }
     attr_reader :current_total_discounts
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :current_total_discounts_set
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :current_total_duties_set
+
     sig { returns(T.nilable(String)) }
     attr_reader :current_total_price
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :current_total_price_set
+
     sig { returns(T.nilable(String)) }
     attr_reader :current_total_tax
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :current_total_tax_set
+
     sig { returns(T.nilable(Customer)) }
     attr_reader :customer
+
     sig { returns(T.nilable(String)) }
     attr_reader :customer_locale
+
     sig { returns(T.nilable(T::Array[T::Hash[T.untyped, T.untyped]])) }
     attr_reader :discount_applications
+
     sig { returns(T.nilable(T::Array[DiscountCode])) }
     attr_reader :discount_codes
+
     sig { returns(T.nilable(String)) }
     attr_reader :email
+
     sig { returns(T.nilable(T::Boolean)) }
     attr_reader :estimated_taxes
+
     sig { returns(T.nilable(String)) }
     attr_reader :financial_status
+
     sig { returns(T.nilable(String)) }
     attr_reader :fulfillment_status
+
     sig { returns(T.nilable(T::Array[Fulfillment])) }
     attr_reader :fulfillments
+
     sig { returns(T.nilable(String)) }
     attr_reader :gateway
+
     sig { returns(T.nilable(Integer)) }
     attr_reader :id
+
     sig { returns(T.nilable(String)) }
     attr_reader :landing_site
+
     sig { returns(T.nilable(Integer)) }
     attr_reader :location_id
+
     sig { returns(T.nilable(Integer)) }
     attr_reader :merchant_of_record_app_id
+
     sig { returns(T.nilable(String)) }
     attr_reader :name
+
     sig { returns(T.nilable(String)) }
     attr_reader :note
+
     sig { returns(T.nilable(T::Array[T::Hash[T.untyped, T.untyped]])) }
     attr_reader :note_attributes
+
     sig { returns(T.nilable(Integer)) }
     attr_reader :number
+
     sig { returns(T.nilable(Integer)) }
     attr_reader :order_number
+
     sig { returns(T.nilable(String)) }
     attr_reader :order_status_url
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :original_total_duties_set
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :payment_details
+
     sig { returns(T.nilable(T::Array[String])) }
     attr_reader :payment_gateway_names
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :payment_terms
+
     sig { returns(T.nilable(String)) }
     attr_reader :phone
+
     sig { returns(T.nilable(String)) }
     attr_reader :presentment_currency
+
     sig { returns(T.nilable(String)) }
     attr_reader :processed_at
+
     sig { returns(T.nilable(String)) }
     attr_reader :processing_method
+
     sig { returns(T.nilable(String)) }
     attr_reader :referring_site
+
     sig { returns(T.nilable(T::Array[Refund])) }
     attr_reader :refunds
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :shipping_address
+
     sig { returns(T.nilable(T::Array[T::Hash[T.untyped, T.untyped]])) }
     attr_reader :shipping_lines
+
     sig { returns(T.nilable(String)) }
     attr_reader :source_identifier
+
     sig { returns(T.nilable(String)) }
     attr_reader :source_name
+
     sig { returns(T.nilable(String)) }
     attr_reader :source_url
-    sig { returns(T.nilable(Float)) }
+
+    sig { returns(T.nilable(String)) }
     attr_reader :subtotal_price
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :subtotal_price_set
+
     sig { returns(T.nilable(String)) }
     attr_reader :tags
+
     sig { returns(T.nilable(T::Array[T::Hash[T.untyped, T.untyped]])) }
     attr_reader :tax_lines
+
     sig { returns(T.nilable(T::Boolean)) }
     attr_reader :taxes_included
+
     sig { returns(T.nilable(T::Boolean)) }
     attr_reader :test
+
     sig { returns(T.nilable(String)) }
     attr_reader :token
+
     sig { returns(T.nilable(String)) }
     attr_reader :total_discounts
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :total_discounts_set
+
     sig { returns(T.nilable(String)) }
     attr_reader :total_line_items_price
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :total_line_items_price_set
+
     sig { returns(T.nilable(String)) }
     attr_reader :total_outstanding
+
     sig { returns(T.nilable(String)) }
     attr_reader :total_price
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :total_price_set
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :total_shipping_price_set
+
     sig { returns(T.nilable(T.any(String, Float))) }
     attr_reader :total_tax
+
     sig { returns(T.nilable(T::Hash[T.untyped, T.untyped])) }
     attr_reader :total_tax_set
+
     sig { returns(T.nilable(String)) }
     attr_reader :total_tip_received
+
     sig { returns(T.nilable(Integer)) }
     attr_reader :total_weight
+
     sig { returns(T.nilable(String)) }
     attr_reader :updated_at
+
     sig { returns(T.nilable(Integer)) }
     attr_reader :user_id
 
@@ -281,7 +359,7 @@ module ShopifyAPI
         params(
           id: T.any(Integer, String),
           fields: T.untyped,
-          session: Auth::Session
+          session: Auth::Session,
         ).returns(T.nilable(Order))
       end
       def find(
@@ -291,8 +369,8 @@ module ShopifyAPI
       )
         result = base_find(
           session: session,
-          ids: {id: id},
-          params: {fields: fields},
+          ids: { id: id },
+          params: { fields: fields },
         )
         T.cast(result[0], T.nilable(Order))
       end
@@ -300,7 +378,7 @@ module ShopifyAPI
       sig do
         params(
           id: T.any(Integer, String),
-          session: Auth::Session
+          session: Auth::Session,
         ).returns(T.untyped)
       end
       def delete(
@@ -311,7 +389,7 @@ module ShopifyAPI
           http_method: :delete,
           operation: :delete,
           session: session,
-          ids: {id: id},
+          ids: { id: id },
           params: {},
         )
       end
@@ -333,7 +411,7 @@ module ShopifyAPI
           fulfillment_status: T.untyped,
           fields: T.untyped,
           session: Auth::Session,
-          kwargs: T.untyped
+          kwargs: T.untyped,
         ).returns(T::Array[Order])
       end
       def all(
@@ -357,7 +435,8 @@ module ShopifyAPI
         response = base_find(
           session: session,
           ids: {},
-          params: {ids: ids, limit: limit, since_id: since_id, created_at_min: created_at_min, created_at_max: created_at_max, updated_at_min: updated_at_min, updated_at_max: updated_at_max, processed_at_min: processed_at_min, processed_at_max: processed_at_max, attribution_app_id: attribution_app_id, status: status, financial_status: financial_status, fulfillment_status: fulfillment_status, fields: fields}.merge(kwargs).compact,
+          params: { ids: ids, limit: limit, since_id: since_id, created_at_min: created_at_min,
+                    created_at_max: created_at_max, updated_at_min: updated_at_min, updated_at_max: updated_at_max, processed_at_min: processed_at_min, processed_at_max: processed_at_max, attribution_app_id: attribution_app_id, status: status, financial_status: financial_status, fulfillment_status: fulfillment_status, fields: fields, }.merge(kwargs).compact,
         )
 
         T.cast(response, T::Array[Order])
@@ -373,7 +452,7 @@ module ShopifyAPI
           financial_status: T.untyped,
           fulfillment_status: T.untyped,
           session: Auth::Session,
-          kwargs: T.untyped
+          kwargs: T.untyped,
         ).returns(T.untyped)
       end
       def count(
@@ -392,12 +471,12 @@ module ShopifyAPI
           operation: :count,
           session: session,
           ids: {},
-          params: {created_at_min: created_at_min, created_at_max: created_at_max, updated_at_min: updated_at_min, updated_at_max: updated_at_max, status: status, financial_status: financial_status, fulfillment_status: fulfillment_status}.merge(kwargs).compact,
+          params: { created_at_min: created_at_min, created_at_max: created_at_max, updated_at_min: updated_at_min,
+                    updated_at_max: updated_at_max, status: status, financial_status: financial_status, fulfillment_status: fulfillment_status, }.merge(kwargs).compact,
           body: {},
           entity: nil,
         )
       end
-
     end
 
     sig do
@@ -409,7 +488,7 @@ module ShopifyAPI
         email: T.untyped,
         refund: T.untyped,
         body: T.untyped,
-        kwargs: T.untyped
+        kwargs: T.untyped,
       ).returns(T.untyped)
     end
     def cancel(
@@ -426,8 +505,9 @@ module ShopifyAPI
         http_method: :post,
         operation: :cancel,
         session: @session,
-        ids: {id: @id},
-        params: {amount: amount, currency: currency, restock: restock, reason: reason, email: email, refund: refund}.merge(kwargs).compact,
+        ids: { id: @id },
+        params: { amount: amount, currency: currency, restock: restock, reason: reason, email: email,
+                  refund: refund, }.merge(kwargs).compact,
         body: body,
         entity: self,
       )
@@ -436,7 +516,7 @@ module ShopifyAPI
     sig do
       params(
         body: T.untyped,
-        kwargs: T.untyped
+        kwargs: T.untyped,
       ).returns(T.untyped)
     end
     def close(
@@ -447,7 +527,7 @@ module ShopifyAPI
         http_method: :post,
         operation: :close,
         session: @session,
-        ids: {id: @id},
+        ids: { id: @id },
         params: {}.merge(kwargs).compact,
         body: body,
         entity: self,
@@ -457,7 +537,7 @@ module ShopifyAPI
     sig do
       params(
         body: T.untyped,
-        kwargs: T.untyped
+        kwargs: T.untyped,
       ).returns(T.untyped)
     end
     def open(
@@ -468,12 +548,11 @@ module ShopifyAPI
         http_method: :post,
         operation: :open,
         session: @session,
-        ids: {id: @id},
+        ids: { id: @id },
         params: {}.merge(kwargs).compact,
         body: body,
         entity: self,
       )
     end
-
   end
 end
